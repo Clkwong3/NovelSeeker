ab4af201da9a8210f785ea6a96eb44c316290e37
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-14 00:18:48 -0800

    Schemas - Created this folder to hold the files refactored from the user-controller.js

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,111 @@
+// Import necessary modules
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+// Define GraphQL resolvers for Query and Mutation
+const resolvers = {
+  Query: {
+    // Resolver for the 'getSingleUser' query to retrieve a user by ID or username
+    getSingleUser: async ({ user = null, params }) => {
+      // Find a user by ID or username
+      const foundUser = await User.findOne({
+        $or: [
+          { _id: user ? user._id : params.id },
+          { username: params.username },
+        ],
+      });
+
+      // Throw an error if user is not found
+      if (!foundUser) {
+        throw new Error("Cannot find a user with this id!");
+      }
+
+      // Return the found user
+      return foundUser;
+    },
+  },
+
+  Mutation: {
+    // Resolver for the 'createUser' mutation to create a new user
+    createUser: async ({ body }) => {
+      // Create a new user
+      const user = await User.create(body);
+
+      // Throw an error if user creation fails
+      if (!user) {
+        throw new Error("Something is wrong!");
+      }
+
+      // Generate a token for the new user
+      const token = signToken(user);
+
+      // Return the token and user information
+      return { token, user };
+    },
+
+    // Resolver for the 'login' mutation to authenticate and generate a token for a user
+    login: async ({ body }) => {
+      // Find a user by username or email
+      const user = await User.findOne({
+        $or: [{ username: body.username }, { email: body.email }],
+      });
+
+      // Throw an error if user is not found
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+
+      // Check if the provided password is correct
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      // Throw an error if the password is incorrect
+      if (!correctPw) {
+        throw new Error("Wrong password!");
+      }
+
+      // Generate a token for the authenticated user
+      const token = signToken(user);
+
+      // Return the token and user information
+      return { token, user };
+    },
+
+    // Resolver for the 'saveBook' mutation to add a book to a user's saved books
+    saveBook: async ({ user, body }) => {
+      try {
+        // Add the book to the user's saved books and return the updated user
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        // Log and throw an error if saving the book fails
+        console.error(err);
+        throw new Error("Failed to save the book.");
+      }
+    },
+
+    // Resolver for the 'deleteBook' mutation to remove a book from a user's saved books
+    deleteBook: async ({ user, params }) => {
+      // Remove the specified book from the user's saved books and return the updated user
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+
+      // Throw an error if the user is not found
+      if (!updatedUser) {
+        throw new Error("Couldn't find user with this id!");
+      }
+
+      // Return the updated user
+      return updatedUser;
+    },
+  },
+};
+
+// Export the resolvers for use in the Apollo Server
+module.exports = resolvers;
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookInput: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: ID
+    image: String
+    link: String
+  }
+`;
+
+module.exports = typeDefs;
