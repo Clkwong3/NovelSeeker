2ce4f949f5a2fa6670b229583248355a7bf6be45
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-17 19:03:00 -0800

    Utils - created files for mutations and queries. Brought the code from Apollo Server Graphql

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation SaveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation DeleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
